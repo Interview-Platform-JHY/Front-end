4f45bef2ed1560e77a15410f59d4702a47924131
서주희 <maywngml@naver.com>
2023-11-04 16:05:00 +0900

    [Add] 지원자 이력서 컴포넌트 구현

---
+++ src/components/common/resume.tsx
@@ -0,0 +1,139 @@
+export default function Resume() {
+  return (
+    <div>
+      <h3 className='mb-6 text-xl font-bold'>남예준 이력서</h3>
+      <div className='mb-6'>
+        <h4 className='mb-4 text-lg font-bold'>기본 인적사항</h4>
+        <div className='mb-3'>
+          <span className='mr-2 font-bold'>이름</span>
+          <span className='mr-3'>남예준</span>
+          <span className='mr-2 font-bold'>생년월일</span>
+          <span>2001.09.12</span>
+        </div>
+        <div>
+          <span className='mr-2 font-bold'>이메일</span>
+          <span className='mr-3'>yejun@plave.com</span>
+          <span className='mr-2 font-bold'>성별</span>
+          <span className='mr-3'>남</span>
+          <span className='mr-2 font-bold'>국적</span>
+          <span>카엘룸</span>
+        </div>
+      </div>
+      <div className='mb-6'>
+        <h4 className='mb-4 text-lg font-bold'>학력사항</h4>
+        <div className='mb-3'>
+          <span className='mr-2 font-bold'>구분</span>
+          <span className='mr-3'>고등학교</span>
+          <span className='mr-2 font-bold'>학교명</span>
+          <span>블래스트 고등학교</span>
+        </div>
+        <div className='mb-3'>
+          <span className='mr-2 font-bold'>재학기간</span>
+          <span className='mr-3'>0000.00 ~ 0000.00</span>
+          <span className='mr-2 font-bold'>졸업구분</span>
+          <span>졸업</span>
+        </div>
+        <div className='mb-3'>
+          <span className='mr-2 font-bold'>구분</span>
+          <span className='mr-3'>대학교</span>
+          <span className='mr-2 font-bold'>학교명</span>
+          <span className='mr-3'>블래스트 대학교</span>
+          <span className='mr-2 font-bold'>전공</span>
+          <span className='mr-3'>실용음악학과</span>
+          <span className='mr-2 font-bold'>부전공</span>
+          <span>연극영화과</span>
+        </div>
+        <div className='mb-3'>
+          <span className='mr-2 font-bold'>학점</span>
+          <span className='mr-3'>3.75 / 4.5</span>
+          <span className='mr-2 font-bold'>재학기간</span>
+          <span className='mr-3'>0000.00 ~ 0000.00</span>
+          <span className='mr-2 font-bold'>졸업구분</span>
+          <span>졸업</span>
+        </div>
+        <div className='mb-3'>
+          <span className='mr-2 font-bold'>구분</span>
+          <span className='mr-3'>대학원</span>
+          <span className='mr-2 font-bold'>학교명</span>
+          <span className='mr-3'>블래스트 대학교</span>
+          <span className='mr-2 font-bold'>전공</span>
+          <span className='mr-3'>실용음악학과</span>
+          <span className='mr-2 font-bold'>부전공</span>
+          <span>연극영화과</span>
+        </div>
+        <div className='mb-3'>
+          <span className='mr-2 font-bold'>학점</span>
+          <span className='mr-3'>3.75 / 4.5</span>
+          <span className='mr-2 font-bold'>재학기간</span>
+          <span className='mr-3'>0000.00 ~ 0000.00</span>
+          <span className='mr-2 font-bold'>졸업구분</span>
+          <span>졸업</span>
+        </div>
+      </div>
+      <div className='mb-6'>
+        <h4 className='mb-4 text-lg font-bold'>경력사항</h4>
+        <div className='mb-3'>
+          <span className='mr-2 font-bold'>회사명</span>
+          <span className='mr-3'>블래스트</span>
+          <span className='mr-2 font-bold'>부서명</span>
+          <span className='mr-3'>플레이브</span>
+          <span className='mr-2 font-bold'>근무기간</span>
+          <span>0000.00 ~ 0000.00</span>
+        </div>
+        <div className='mb-3'>
+          <span className='mr-2 font-bold'>직위</span>
+          <span className='mr-3'>리더</span>
+          <span className='mr-2 font-bold'>재직상태</span>
+          <span className='mr-3'>재직중</span>
+          <span className='mr-2 font-bold'>업무명</span>
+          <span>플레이브 활동</span>
+        </div>
+        <div>
+          <span className='mr-2 font-bold'>업무상세</span>
+          <span className='mr-3'>리더 및 메인보컬</span>
+        </div>
+      </div>
+      <div className='mb-6'>
+        <h4 className='mb-4 text-lg font-bold'>자격사항</h4>
+        <div className='mb-3'>
+          <span className='mr-2 font-bold'>자격증명</span>
+          <span className='mr-3'>보컬기사</span>
+          <span className='mr-2 font-bold'>자격증 등급</span>
+          <span className='mr-3'>기사</span>
+          <span className='mr-2 font-bold'>자격증 기관</span>
+          <span className='mr-3'>아스테룸 자격증 기관</span>
+        </div>
+        <div>
+          <span className='mr-2 font-bold'>자격증 취득 일자</span>
+          <span className='mr-3'>0000.00.00</span>
+          <span className='mr-2 font-bold'>자격증 번호</span>
+          <span>12312123232</span>
+        </div>
+      </div>
+      <div className='mb-6'>
+        <h4 className='mb-4 text-lg font-bold'>수상사항</h4>
+        <div className='mb-3'>
+          <span className='mr-2 font-bold'>수상명</span>
+          <span className='mr-3'>00상</span>
+          <span className='mr-2 font-bold'>수상 등급</span>
+          <span className='mr-3'>대상</span>
+          <span className='mr-2 font-bold'>수상 기관</span>
+          <span className='mr-3'>00 기관</span>
+        </div>
+        <div>
+          <span className='mr-2 font-bold'>수상 일자</span>
+          <span className='mr-3'>0000.00.00</span>
+          <span className='mr-2 font-bold'>수상 내용</span>
+          <span>~~하여 수상함</span>
+        </div>
+      </div>
+      <div>
+        <h4 className='mb-4 text-lg font-bold'>자기소개서</h4>
+        <p className='mb-2 font-bold'>[필수] ~~한 경험을 적으시오.</p>
+        <p className='mb-3'>[필수] ~~한 경험을 적으시오.</p>
+        <p className='mb-2 font-bold'>[필수] ~~한 경험을 적으시오.</p>
+        <p className='mb-3'>[필수] ~~한 경험을 적으시오.</p>
+      </div>
+    </div>
+  );
+}
